06dbfdb574e93daab47af5bea97af7bf26d45b07
Alex <01210133@pw.edu.pl>
2026-03-26 17:19:38 +0100

    add main execution file with CommandProcessor setup

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "CommandProcessor.hpp"
+
+int main() {
+    std::cout << "Redis Clone is starting..." << std::endl;
+
+    CommandProcessor processor;
+
+    return 0;
+}

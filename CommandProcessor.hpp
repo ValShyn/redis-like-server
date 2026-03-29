40eae34cc76d11adafec397b2cf6daca12bc1dc3
Alex <01210133@pw.edu.pl>
2026-03-29 18:25:23 +0200

    add documentation for execute method in CommandProcessor

--- CommandProcessor.hpp
+++ CommandProcessor.hpp
@@ -4,5 +4,8 @@
 
 class CommandProcessor {
 public:
+    /**
+     * Takes a vector of command parts and returns a RESP-formatted response string.
+     */
     std::string execute(const std::vector<std::string>& command);
 };

b1da751e1e6d6b07001afd1e71597cc79f889b12
Alex <01210133@pw.edu.pl>
2026-03-29 19:41:13 +0200

    create Database class header with set and get methods.

---
+++ Database.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <unordered_map>
+#include <string>
+
+class Database{
+    private:
+    std::unordered_map<std::string, std::string> db;
+
+    public:
+    void set(const std::string& key, const std::string& value);
+    std::string get(const std::string& key) const;
+};

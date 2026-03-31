f747d888c7d8dfc1e2d615419dcc856ceda1407f
Alex <01210133@pw.edu.pl>
2026-03-31 17:57:42 +0200

    add Database storage to CommandProcessor header

--- CommandProcessor.hpp
+++ CommandProcessor.hpp
@@ -1,8 +1,13 @@
 #pragma once
 #include <string>
 #include <vector>
+#include "Database.hpp"
+
 
 class CommandProcessor {
+private:
+    Database db;
+
 public:
     /**
      * Takes a vector of command parts and returns a RESP-formatted response string.

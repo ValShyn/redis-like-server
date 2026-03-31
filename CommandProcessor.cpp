587a2454a9c052f0bd262256aa77e0335924e02f
Alex <01210133@pw.edu.pl>
2026-03-31 17:58:29 +0200

    implement SET command with the validation of number of arguments

--- CommandProcessor.cpp
+++ CommandProcessor.cpp
@@ -14,6 +14,17 @@ std::string CommandProcessor::execute(const std::vector<std::string>& command) {
         return "+PONG\r\n";
     }
 
+    if(cmd == "SET" || cmd == "set"){
+
+        // SET command requires 3 arguments
+        if(command.size() != 3){
+            return "-ERR wrong number of arguments for 'set' command\r\n";
+        }
+
+        // if all is good, we set new value
+        this -> db.set(command[1], command[2]);
+        return "+OK\r\n";
+    }
     // Default response for unimplemented or unknown commands
     return "-ERR command not implemented yet\r\n";
 }

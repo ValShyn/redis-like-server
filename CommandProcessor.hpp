cb2093be8c97aae2fd41f8f13e8e4afdf0905500
Valentine Shynkovich <valentin110707@gmail.com>
2026-04-02 01:40:44 +0200

    Change .gitignore

--- CommandProcessor.hpp
+++ CommandProcessor.hpp
@@ -1,8 +1,16 @@
 #pragma once
 #include <string>
 #include <vector>
+#include "Database.hpp"
+
 
 class CommandProcessor {
+private:
+    Database db;
+
 public:
+    /**
+     * Takes a vector of command parts and returns a RESP-formatted response string.
+     */
     std::string execute(const std::vector<std::string>& command);
 };

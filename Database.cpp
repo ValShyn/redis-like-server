cc155d98b3c40b1fc65acc802c7f9a9f2d92a64c
Alex <01210133@pw.edu.pl>
2026-04-01 18:17:49 +0200

    implement incr with string-to-long conversion and error handling

--- Database.cpp
+++ Database.cpp
@@ -30,3 +30,30 @@ bool Database::exists(const std::string& key) const{
     }
     return false;
 }
+
+long long Database::incr(const std::string& key){
+    auto elem = this -> db.find(key);
+
+    // if key is not found, add it to db with value of 1
+    if(elem == this -> db.end()){
+        set(key, "1");
+        return 1;
+    }
+
+    // if the key is found and is convertable to long long
+    // increase it and return
+    try{
+        // std::stoll() to convert string to long long
+        long long elem_value = std::stoll(elem -> second);
+        set(key, std::to_string(++elem_value));
+        return elem_value;
+    }
+
+    // throw an error if we catch an error
+    catch(std::out_of_range&){
+        throw std::runtime_error("not an int");
+    }
+    catch(std::invalid_argument&){
+        throw std::runtime_error("not an int");
+    }
+}
